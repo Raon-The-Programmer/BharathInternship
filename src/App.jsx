b25baf1fc636611c414b5890b4ab4f2e61c879c1
Rahul John <msdrahuljohn@gmail.com>
2023-12-30 18:55:08 +0530

    formik forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { ErrorMessage,Field, Form, Formik } from 'formik'
 import React from 'react'
-
+function ContactForm(){
+  return(
+    <Formik initialValues={{name:'',email:'',message:''}}>
+      <Form>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <Field type='text' name='name'></Field>
+          <ErrorMessage name='name' component='div'></ErrorMessage>
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <Field type='email' name='email'></Field>
+          <ErrorMessage name='email' component='div'></ErrorMessage>
+        </div>
+        <div>
+          <label htmlFor='message'>Message</label>
+          <Field as='textarea' name='message'></Field>
+          <ErrorMessage name='message' component='div'></ErrorMessage>
+        </div>
+        <button type='submit'>Submit</button>
+      </Form>
+    </Formik>
+  )
+}
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <ContactForm />
+    </div>
   )
 }
 
