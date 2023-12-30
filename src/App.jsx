2bef13ac2ab9789be4287e9c2ab939f503e73ee1
Rahul John <msdrahuljohn@gmail.com>
2023-12-30 19:21:48 +0530

    fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Routes,BrowserRouter as Router,Route,Link } from 'react-router-dom'
 
 const App = () => {
+  const[data,setData] = useState([])
+  const fetchData = async()=>{
+    try{const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    setData(res.data)}
+    catch(err){
+      console.log('error occured while fetching: ',err)
+    }
+    
+  }
+  useEffect(()=>{fetchData()},[])
+  
+
   return (
-    <div>App</div>
+    <div>
+    <h1>APP</h1>
+    <ul>
+      {data.map((item)=>(
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+    </div>
   )
 }
 
